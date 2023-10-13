8bb158dc5bbe8cd4d053aac9c96301bfa2d86cce
kkkouo <141680819+kkkouo@users.noreply.github.com>
2023-10-13 20:14:19 +0800

    Add files via upload

---
+++ sagj/static/js/m3u8Links.js
@@ -0,0 +1,31 @@
+
+
+var m3u8Links = [
+  "https://long.longyuandingyi.com/videos2/9c08f421dd13fad5d9987f0dca0c8633/9c08f421dd13fad5d9987f0dca0c8633.m3u8?auth_key=1696350408-0-0-c7a485fc0e1fd9d1794d70e6ebf8e347&v=2",
+"https://long.longyuandingyi.com/watch9/77c8178884c7890649415f4b8c56e3bf/77c8178884c7890649415f4b8c56e3bf.m3u8?auth_key=1696350408-0-0-7b28ee3426d93343a1206e7a4f21e6d4&v=2",
+"https://long.longyuandingyi.com/watch9/5135432cbdfcb25dc6e86ed358dbd06f/5135432cbdfcb25dc6e86ed358dbd06f.m3u8?auth_key=1696350408-0-0-ad7c8733a690e9d81485e675effba05b&v=2",
+"https://long.longyuandingyi.com/watch9/2c45825bc6989c449d5e6501da7e431f/2c45825bc6989c449d5e6501da7e431f.m3u8?auth_key=1696350408-0-0-a7c4e799f701a0d70dcb8f70c8ee7c1f&v=2",
+"https://long.longyuandingyi.com/watch9/7d4991d7d36b9e1965c250090c95e9b6/7d4991d7d36b9e1965c250090c95e9b6.m3u8?auth_key=1696350408-0-0-83a2a2828ede4069e5cb05f22cdd3a58&v=2",
+"https://long.longyuandingyi.com/watch9/7120296ac71af387ebee0d7af6f66985/7120296ac71af387ebee0d7af6f66985.m3u8?auth_key=1696350408-0-0-6336152c471a2b73ac9950ead2968b2d&v=2",
+"https://long.longyuandingyi.com/watch9/28d4e695048666344451ff1e6d5ba310/28d4e695048666344451ff1e6d5ba310.m3u8?auth_key=1696350408-0-0-b674d44284940d5a4f3c9e65852773a1&v=2",
+"https://long.longyuandingyi.com/watch9/e18138bd9f1397c276c0bc2ce15afd5d/e18138bd9f1397c276c0bc2ce15afd5d.m3u8?auth_key=1696350408-0-0-60fdb644d9d73824440d1a6b8d2a2e95&v=2",
+"https://long.longyuandingyi.com/watch9/d1cb60c852c7d9da9ebf132115d4e676/d1cb60c852c7d9da9ebf132115d4e676.m3u8?auth_key=1696350408-0-0-55661e3056820f0c0e738953cc2ef5fe&v=2",
+"https://long.longyuandingyi.com/watch9/5684128708fe816e1a89b397f17b7454/5684128708fe816e1a89b397f17b7454.m3u8?auth_key=1696350408-0-0-f1c31aa67f3f4f4a6ccbd0754d3757c4&v=2",
+"https://long.longyuandingyi.com/watch9/b11ffabcaa6ae1042a09946a689e4f43/b11ffabcaa6ae1042a09946a689e4f43.m3u8?auth_key=1696350408-0-0-32f839a7c2f5a28e6f56d9cf6ce88125&v=2",
+"https://long.longyuandingyi.com/watch9/1b533ab5fbd5a55f24b824c404dc7465/1b533ab5fbd5a55f24b824c404dc7465.m3u8?auth_key=1696350408-0-0-fdd55feb2828d3ba310ea7fa36798993&v=2",
+"https://long.longyuandingyi.com/watch9/d18af5335095c250762d244b8836ecb9/d18af5335095c250762d244b8836ecb9.m3u8?auth_key=1696350408-0-0-902b8ffb117e3519f98712df87d5c926&v=2",
+"https://long.longyuandingyi.com/watch9/ada159bdfe0526c0fdfbded09d950fe9/ada159bdfe0526c0fdfbded09d950fe9.m3u8?auth_key=1696350408-0-0-c5a43e3bcc0ab1424263bac34cd8b594&v=2",
+"https://long.longyuandingyi.com/watch9/982177f9fd1ca7ebd03c4a3044380f94/982177f9fd1ca7ebd03c4a3044380f94.m3u8?auth_key=1696350408-0-0-03aaa462715261efcd24918757e947d0&v=2",
+"https://long.longyuandingyi.com/watch9/6e78512e7f46c5dddc1646fe1bae5d92/6e78512e7f46c5dddc1646fe1bae5d92.m3u8?auth_key=1696350409-0-0-cadb7d2df20c3911c13a773614f1b5ce&v=2",
+"https://long.longyuandingyi.com/watch9/fe669e57db78f33c346ad6201afba554/fe669e57db78f33c346ad6201afba554.m3u8?auth_key=1696350409-0-0-d62013bebf55663d14c11b899a1e9a61&v=2",
+"https://long.longyuandingyi.com/watch9/7ac57d36853f765502d36bff7d5fbeb1/7ac57d36853f765502d36bff7d5fbeb1.m3u8?auth_key=1696350409-0-0-21e5fba416b31e4c74b8314358aadc43&v=2",
+"https://long.longyuandingyi.com/watch9/b1b243e414c763601c5b36c34d923655/b1b243e414c763601c5b36c34d923655.m3u8?auth_key=1696350409-0-0-c0be8ff3ff8e4fb8932d91248e207006&v=2",
+"https://long.longyuandingyi.com/watch9/476bc481652b22e6dbb5d13f058f1368/476bc481652b22e6dbb5d13f058f1368.m3u8?auth_key=1696350409-0-0-9e43d54290aa45516c95d4c4ca8fab6c&v=2",
+"https://long.longyuandingyi.com/watch9/ed2e7bc10bb446a162eefe675d4f9ee2/ed2e7bc10bb446a162eefe675d4f9ee2.m3u8?auth_key=1696350409-0-0-54aeff903704c59b9a457e0f650aea46&v=2",
+"https://long.longyuandingyi.com/watch9/8b173e4abdeaaa8f3b3980341855f451/8b173e4abdeaaa8f3b3980341855f451.m3u8?auth_key=1696350409-0-0-fdfc079f9da5398aab8ba0b7695addd6&v=2",
+"https://long.longyuandingyi.com/watch9/d660d620aeac1e16e2dd18e087f330f4/d660d620aeac1e16e2dd18e087f330f4.m3u8?auth_key=1696350409-0-0-ec8ce7d17d7a077fd036b6e43ecde952&v=2",
+"https://long.longyuandingyi.com/watch9/0ee5a6f48f2a59b02ca2cecd597f7c60/0ee5a6f48f2a59b02ca2cecd597f7c60.m3u8?auth_key=1696350409-0-0-b4e84815bd7b926e4d27e681859a7f83&v=2",
+"https://long.longyuandingyi.com/watch9/07a27bc110bf1c43366e9a509ce9386b/07a27bc110bf1c43366e9a509ce9386b.m3u8?auth_key=1696350409-0-0-36ce423debfdc23312f8ed1e201dbb1f&v=2",
+"https://long.longyuandingyi.com/watch9/7dbd05589b72066bb6d5146a494ca912/7dbd05589b72066bb6d5146a494ca912.m3u8?auth_key=1696350409-0-0-ce5dfb6656278a5be57790fcd278b4ff&v=2",
+"https://long.longyuandingyi.com/watch9/33044929f54f91a7604f34e2fbd69166/33044929f54f91a7604f34e2fbd69166.m3u8?auth_key=1696350409-0-0-62e9dd0274f9bf4e6293d4196a1a22ae&v=2"
+];
